163fe6975ab656ef2dfd2170d850a7d93bc5c71f
Y-ash-Thakur <yasht0509@gmail.com>
2026-02-01 20:47:34 +0530

    Routing basics

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,7 @@
+const UserDetails = async ({params}: {params: Promise<{id: string}>}) => {
+    const { id } = await params;
+    return (  
+        <h1>showing details for user {id}</h1>
+    )
+}
+export default UserDetails
